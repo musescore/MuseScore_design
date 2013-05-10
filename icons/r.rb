67224e9abcbda48c16e7ae0042cd29442180ba74
tinman <mazelm@gmail.com>
2013-05-10 12:08:57 +0200

    / ‘icons/r.rb’

--- icons/r.rb
+++ icons/r.rb
@@ -23,9 +23,9 @@ def chopSVG(icon)
 		#system(cmd)
 		svgcrop = Document.new(File.new(icon[:file], 'r'))
 		svgcrop.root.each_element("//rect") do |rect| 
-			w = ((rect.attributes["width"].to_f * 10).round / 10.0).to_i #get rid of 16 vs 15.99999 
+			w = ((rect.attributes["width"].to_f * 10).round / 10.0).to_i #get rid of 24 vs 23.99999 
 			h = ((rect.attributes["width"].to_f * 10).round / 10.0).to_i #Inkscape bugs
-			if w == 16 && h == 16
+			if w == 24 && h == 24
 				rect.remove
 			end
 		end
