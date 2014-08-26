db6937ef457f6a681a24abf49599b5a4515c5e42
mirek2 <mazelm@gmail.com>
2014-08-26 13:23:01 +0200

    / ‘icons/make-icons.sh’

---
+++ icons/make-icons.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+~/git/svgo/bin/svgo -f ~/SparkleShare/MuseScore_design/icons/16/actions
+~/git/svgo/bin/svgo -f ~/SparkleShare/MuseScore_design/icons/24/actions
+~/git/svgo/bin/svgo -f ~/SparkleShare/MuseScore_design/icons/48/actions
