7cbe2cc36c36a7ddd70e08de9f10a51037cb0c77
mirek2 <mazelm@gmail.com>
2014-09-07 14:11:57 +0200

    / ‘icons/make-icons.sh’

--- icons/make-icons.sh
+++ icons/make-icons.sh
@@ -5,6 +5,12 @@
 for f in ./*/actions/*.svg;
 	do inkscape -f $f --vacuum-defs -l $f;
 done
+inkscape:connector-curvature="0"
+
+sed -i 's/inkscape:connector-curvature="0"//g' ./16/actions/*
+sed -i 's/inkscape:connector-curvature="0"//g' ./24/actions/*
+sed -i 's/inkscape:connector-curvature="0"//g' ./48/actions/*
+
 ~/git/svgo/bin/svgo -f ./16/actions
 ~/git/svgo/bin/svgo -f ./24/actions
 ~/git/svgo/bin/svgo -f ./48/actions
