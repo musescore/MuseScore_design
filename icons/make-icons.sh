fef19824eeeccc04d1635fe8e219f1abdcd5b9f5
mirek2 <mazelm@gmail.com>
2014-09-07 13:54:34 +0200

    / ‘icons/make-icons.sh’

--- icons/make-icons.sh
+++ icons/make-icons.sh
@@ -2,6 +2,9 @@
 ./16/r.rb
 ./24/r.rb
 ./48/r.rb
+for f in ./*/actions/*.svg;
+	do inkscape -f $f --vacuum-defs -l $f;
+done
 ~/git/svgo/bin/svgo -f ./16/actions
 ~/git/svgo/bin/svgo -f ./24/actions
 ~/git/svgo/bin/svgo -f ./48/actions
