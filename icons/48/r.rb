8a636dde6af642f37cc1dd6dc404892a582ef37d
mirek2 <mazelm@gmail.com>
2014-09-09 01:35:23 +0200

    / ‘icons/48/r.rb’

--- icons/48/r.rb
+++ icons/48/r.rb
@@ -29,7 +29,7 @@ def chopSVG(icon)
 				rect.remove
 			end
 		end
-		system("sed "s/<title id='[a-zA-Z1-9]*'>/<title>/g" dialog-question.svg")
+		system("awk '/metadata/{getline; while($0!~/metadata/) {getline}; getline}1' dialog-question.svg > #{icon[:file]}.tmp; rm #{icon[:file]}; mv #{icon[:file]}.tmp #{icon[:file]})
     icon_f = File.new(icon[:file],'w+')
     icon_f.puts svgcrop
     icon_f.close
