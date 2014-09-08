dcbc1e7f5fda379e72075ea8f87f34c01c653c02
mirek2 <mazelm@gmail.com>
2014-09-09 00:52:32 +0200

    / ‘icons/48/r.rb’

--- icons/48/r.rb
+++ icons/48/r.rb
@@ -6,6 +6,7 @@ include REXML
 
 
 INKSCAPE = '/usr/bin/inkscape'
+#INKSCAPE = '/usr/bin/inkscape' # like this works for me, while using `which` inkscape hangs
 SRC = "src.svg"
 
 def chopSVG(icon)
@@ -13,7 +14,7 @@ def chopSVG(icon)
 	unless (File.exists?(icon[:file]) && !icon[:forcerender])
 		FileUtils.cp(SRC,icon[:file]) 
 		puts " >> #{icon[:name]}"
-		cmd = "#{INKSCAPE} -f #{icon[:file]} --vacuum-defs --select #{icon[:id]} --verb=FitCanvasToSelection  --verb=EditInvertInAllLayers "
+		cmd = "#{INKSCAPE} -f #{icon[:file]} --select #{icon[:id]} --verb=FitCanvasToSelection  --verb=EditInvertInAllLayers "
 		cmd += "--verb=EditDelete --verb=EditSelectAll --verb=SelectionUnGroup --verb=SelectionUnGroup --verb=SelectionUnGroup --verb=StrokeToPath --verb=FileVacuum "
 		cmd += "--verb=FileSave --verb=FileClose > /dev/null 2>&1"
 		system(cmd)
@@ -21,11 +22,10 @@ def chopSVG(icon)
 		#cmd = "#{INKSCAPE} -f #{icon[:file]} -z --vacuum-defs -l #{icon[:file]} > /dev/null 2>&1"
 		#system(cmd)
 		svgcrop = Document.new(File.new(icon[:file], 'r'))
-#!	should check opacity instead
 		svgcrop.root.each_element("//rect") do |rect| 
 			w = ((rect.attributes["width"].to_f * 10).round / 10.0).to_i #get rid of 24 vs 23.99999 
 			h = ((rect.attributes["width"].to_f * 10).round / 10.0).to_i #Inkscape bugs
-			if w == 48 && h == 48
+			if w == 24 && h == 24
 				rect.remove
 			end
 		end
@@ -59,4 +59,16 @@ if (ARGV[0].nil?) #render all SVGs
 		end
 	end
   puts "\nrendered all SVGs"
+else #only render the icons passed
+  icons = ARGV
+  ARGV.each do |icon_name|
+  	icon = svg.root.elements["//g[@id='#{icon_name}']"]
+  	dir = "#{icon.parent.attributes['id']}"
+		chopSVG({	:name => icon_name,
+		 					:id => icon.attributes["id"],
+		 					:dir => dir,
+		 					:file => "#{dir}/#{icon_name}.svg",
+		 					:forcerender => true})
+	end
+  puts "\nrendered #{ARGV.length} icons"
 end
