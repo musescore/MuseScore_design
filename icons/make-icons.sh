305cb2400c77fcc59b52408a8d649767f2aec73e
mirek2 <mazelm@gmail.com>
2014-09-09 17:29:49 +0200

    / ‘icons/make-icons.sh’

--- icons/make-icons.sh
+++ icons/make-icons.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i -f simplifySVG.sed 16/src.svg
-sed -i -f simplifySVG.sed 24/src.svg
-sed -i -f simplifySVG.sed 48/src.svg
+sed -f simplifySVG.sed 16/src.svg
+sed -f simplifySVG.sed 24/src.svg
+sed -f simplifySVG.sed 48/src.svg
 ./r.rb 16 24 48
