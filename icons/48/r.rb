79f706c05ee5976cc41e702d548022278199cdeb
mirek2 <mazelm@gmail.com>
2014-09-09 12:48:23 +0200

    / ‘icons/48/r.rb’

--- icons/48/r.rb
+++ icons/48/r.rb
@@ -7,7 +7,7 @@ include REXML
 
 INKSCAPE = '/usr/bin/inkscape'
 #INKSCAPE = '/usr/bin/inkscape' # like this works for me, while using `which` inkscape hangs
-SRC = "~/SparkleShare/MuseScore_design/icons/48/src.svg"
+SRC = "src.svg"
 
 def chopSVG(icon)
 	FileUtils.mkdir_p(icon[:dir]) unless File.exists?(icon[:dir])
